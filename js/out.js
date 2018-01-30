e49bad6130fe47736d6e67c71bdea2d5d549f022
Stanisław Gregor <stasiugregor@gmail.com>
2018-01-30 13:49:02 +0100

    Slider partly working | Section products finished

--- js/out.js
+++ js/out.js
@@ -65,9 +65,43 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1)
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function changeSlides(n) {
+    console.log('Changing slide by' + n);
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "flex";
+}
+
+var nextSlide = document.querySelector('.fa-angle-right');
+nextSlide.addEventListener('click', changeSlides(1));
+
+var prevSlide = document.querySelector('.fa-angle-left');
+prevSlide.addEventListener('click', changeSlides(-1));
 
 /***/ })
 /******/ ]);

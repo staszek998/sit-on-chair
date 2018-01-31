ccee3fca21559057c238f45a948f14bf8e156102
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-01-31 20:52:26 +0100

    Remove unnecessary console logging

--- js/out.js
+++ js/out.js
@@ -76,47 +76,10 @@ __webpack_require__(4);
 /* 1 */
 /***/ (function(module, exports) {
 
-/* var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function changeSlides(n) {
-    console.log('Changing slide by' + n);
-  showSlides((slideIndex += n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "flex";
-}
-
-var nextSlide = document.querySelector('.fa-angle-right');
-nextSlide.addEventListener('click', changeSlides(1));
-
-var prevSlide = document.querySelector('.fa-angle-left');
-prevSlide.addEventListener('click', changeSlides(-1)); */
-
-/* ******************************** */
-
 var nextButton = document.querySelector(".fa-angle-right");
 var prevButton = document.querySelector(".fa-angle-left");
 var lis = document.querySelector('div.slider').querySelectorAll("li");
 var indexOfImg = 0;
-
-console.log(nextButton);
-console.log(prevButton);
-console.log(indexOfImg);
-console.log(lis);
 
 lis[indexOfImg].className += " visible";
 

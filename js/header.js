b365a0bfc3a9df565a1b1e56156c7099425bb7e4
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-01-31 10:54:15 +0100

    Create 2 more slides and make slider working

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+/* var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -26,4 +26,38 @@ var nextSlide = document.querySelector('.fa-angle-right');
 nextSlide.addEventListener('click', changeSlides(1));
 
 var prevSlide = document.querySelector('.fa-angle-left');
-prevSlide.addEventListener('click', changeSlides(-1));
+prevSlide.addEventListener('click', changeSlides(-1)); */
+
+/* ******************************** */
+
+var nextButton = document.querySelector(".fa-angle-right");
+var prevButton = document.querySelector(".fa-angle-left");
+var lis = document.querySelector('div.slider').querySelectorAll("li");
+var indexOfImg = 0;
+
+console.log(nextButton);
+console.log(prevButton);
+console.log(indexOfImg);
+console.log(lis);
+
+lis[indexOfImg].className += " visible";
+
+nextButton.addEventListener("click", function() {
+    console.log("Next button CLICK");
+    lis[indexOfImg].classList.toggle("visible");
+    indexOfImg++;
+    if (indexOfImg == lis.length) {
+        indexOfImg = 0;
+    }
+    lis[indexOfImg].classList.toggle("visible");
+})
+
+prevButton.addEventListener("click", function() {
+    console.log("Prev button CLICK")
+    lis[indexOfImg].classList.toggle("visible");
+    indexOfImg--;
+    if (indexOfImg == -1) {
+        indexOfImg = (lis.length - 1);
+    }
+    lis[indexOfImg].classList.toggle("visible");
+})
